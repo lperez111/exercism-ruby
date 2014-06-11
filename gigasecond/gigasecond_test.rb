06d71a69f5c6ae15b3bc0945eff583441e078353
Luis Perez <lperez111@gmail.com>
2014-06-10 22:53:04 -0500

    first iteration of gigasecond problem

--- gigasecond/gigasecond_test.rb
+++ gigasecond/gigasecond_test.rb
@@ -11,22 +11,21 @@ class GigasecondTest < MiniTest::Unit::TestCase
   end
 
   def test_2
-    skip
+    # skip
     gs = Gigasecond.new(Date.new(1977, 6, 13))
     assert_equal Date.new(2009, 2, 19), gs.date
   end
 
   def test_3
-    skip
+    # skip
     gs = Gigasecond.new(Date.new(1959, 7, 19))
     assert_equal Date.new(1991, 3, 27), gs.date
   end
 
   def test_yourself
-    skip
-    your_birthday = Date.new(year, month, day)
-    gs = Gigasecond.new(your_birthday)
-    assert_equal Date.new(2009, 1, 31), gs.date
+    # skip
+    gs = Gigasecond.new(Date.new(1987, 11, 28))
+    assert_equal Date.new(2019, 8, 6), gs.date
   end
 
 end
