442cf8ce926503a6281453ea557cd3fcbae59166
Luis Perez <lperez111@gmail.com>
2014-08-13 14:01:15 -0500

    Completed word count problem

--- word-count/phrase.rb
+++ word-count/phrase.rb
@@ -4,6 +4,13 @@ class Phrase
 	end
 
 	def word_count
-		
+		hash = Hash.new(0)
+		@string.downcase.scan(/[a-z0-9']+/) do |word|
+			hash[word] += 1
+		end
+		hash
 	end
 end
+
+phrase = Phrase.new("one fish two fish red fish blue fish")
+phrase.word_count
