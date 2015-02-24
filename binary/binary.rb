5d90d4c2d243ca2994254c1a6c4099cbc6027339
Luis Perez <lperez111@gmail.com>
2015-02-23 20:08:12 -0600

    final version of to_binary method

--- binary/binary.rb
+++ binary/binary.rb
@@ -5,12 +5,23 @@ class Binary
 
 	def to_binary
 		array = []
+		return 0 if is_num? == false
 		until @num == 0
 			array << @num%2
 			@num = @num/2
 		end
-		array.reverse
+		array.reverse.join
+	end
+
+	def to_decimal
+		
+	end
+
+	private
+
+	def is_num?
+		@num == @num.to_i
 	end
 end
 
-Binary.new(2).to_binary
+Binary.new(9).to_binary
