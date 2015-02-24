c39569da48e02267c8c4af0bab402fcf2a22f5f9
Luis Perez <lperez111@gmail.com>
2015-02-23 19:33:21 -0600

    accidently did a to_binary method instead of to_decimal method

--- binary/binary.rb
+++ binary/binary.rb
@@ -1,5 +1,16 @@
 class Binary
+	def initialize(num)
+		@num = num
+	end
 
+	def to_binary
+		array = []
+		until @num == 0
+			array << @num%2
+			@num = @num/2
+		end
+		array.reverse
+	end
 end
 
-Binary.new
+Binary.new(2).to_binary
